e7d878ed64bc8e084a164b414d78188a0761c92a
Challa Sharath <csharath@outlook.com>
2023-02-10 06:03:42 +0530

    automatically get site url instead from env

--- packages/ui/src/AuthForm.js
+++ packages/ui/src/AuthForm.js
@@ -32,12 +32,12 @@ export const AuthForm = ({ type, campaignId, companyId, campaignHandle, affiliat
 
     if(password && password?.length){
       if(type === "signin"){
-        signInFunc = await signInWithPassword({ "email": email, "password": password, "shouldCreateUser": type === "signin" ? false : true, "redirectTo": `${affiliate === true ? process.env.NEXT_PUBLIC_AFFILIATE_SITE_URL : process.env.NEXT_PUBLIC_SITE_URL}/dashboard`});
+        signInFunc = await signInWithPassword({ "email": email, "password": password, "shouldCreateUser": type === "signin" ? false : true, "redirectTo": `${affiliate === true ? process.env.NEXT_PUBLIC_AFFILIATE_SITE_URL : window.location.origin}/dashboard`});
       } else {
-        signInFunc = await signUp({ "email": email, "password": password, "redirectTo": `${affiliate === true ? process.env.NEXT_PUBLIC_AFFILIATE_SITE_URL : process.env.NEXT_PUBLIC_SITE_URL}/dashboard`});
+        signInFunc = await signUp({ "email": email, "password": password, "redirectTo": `${affiliate === true ? process.env.NEXT_PUBLIC_AFFILIATE_SITE_URL : window.location.origin}/dashboard`});
       }
     } else {
-      signInFunc = await signIn({ "email": email, "shouldCreateUser": type === "signin" ? false : true, "redirectTo": `${affiliate === true ? process.env.NEXT_PUBLIC_AFFILIATE_SITE_URL : process.env.NEXT_PUBLIC_SITE_URL}/dashboard`});
+      signInFunc = await signIn({ "email": email, "shouldCreateUser": type === "signin" ? false : true, "redirectTo": `${affiliate === true ? process.env.NEXT_PUBLIC_AFFILIATE_SITE_URL : window.location.origin}/dashboard`});
     }
    
     if(signInFunc?.error){
