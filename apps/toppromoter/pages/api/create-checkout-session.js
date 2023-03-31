d6b801e816057809a2bd64684123b13a21498796
Sharath Challa <csharath@outlook.com>
2023-03-31 18:26:39 +0530

    add logging the user to test stripe checkout

--- apps/toppromoter/pages/api/create-checkout-session.js
+++ apps/toppromoter/pages/api/create-checkout-session.js
@@ -11,6 +11,7 @@ const createCheckoutSession = async (req, res) => {
 
     try {
       const user = await getUser(token, teamId);
+      console.log(user);
       const customer = await createOrRetrieveCustomer({
         id: user.id,
         teamId: user.team_id,
