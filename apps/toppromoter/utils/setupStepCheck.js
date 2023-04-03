9677cb6fd24933b17fa300ba9a1670e16aa1d4be
Sharath Challa <csharath@outlook.com>
2023-04-03 15:40:25 +0530

    setup redirect-uri for localhost domain for stripe connect

--- apps/toppromoter/utils/setupStepCheck.js
+++ apps/toppromoter/utils/setupStepCheck.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useCompany } from './CompanyContext';
 import { useCampaign } from './CampaignContext';
@@ -7,6 +8,7 @@ export default function setupStepCheck(type) {
   const { activeCompany } = useCompany();
   const { userCampaignDetails } = useCampaign();
 
+
   const replaceUrl = (url) => {
     if(router.asPath === `/dashboard/${router?.query?.companyId}${url}`) return false;
     if(router.asPath.includes('/dashboard/stripe-verify')) return false;
@@ -14,36 +16,38 @@ export default function setupStepCheck(type) {
     router.push(`/dashboard/${router?.query?.companyId}${url}`);
   }
 
-  if(activeCompany?.company_id){
-    if(type === 'light'){
-      if(activeCompany?.payment_integration_type === null){
-        replaceUrl('/setup/payment-processor');
-      }
-    
-      if(activeCompany?.payment_integration_type !== null && activeCompany?.company_currency === null){
-        replaceUrl('/setup/currency');
-      }
+  useEffect(() => {
+    if(activeCompany?.company_id){
+      if(type === 'light'){
+        if(activeCompany?.payment_integration_type === null){
+          replaceUrl('/setup/payment-processor');
+        }
+      
+        if(activeCompany?.payment_integration_type !== null && activeCompany?.company_currency === null){
+          replaceUrl('/setup/currency');
+        }
 
-    } else {
-      if(activeCompany?.payment_integration_type === null){
-        replaceUrl('/setup/payment-processor');
-      }
+      } else {
+        if(activeCompany?.payment_integration_type === null){
+          replaceUrl('/setup/payment-processor');
+        }
+      
+        if(activeCompany?.payment_integration_type !== null && activeCompany?.company_currency === null){
+          replaceUrl('/setup/currency');
+        }
+      
+        if(activeCompany?.payment_integration_type !== null && activeCompany?.company_currency !== null && userCampaignDetails?.length === 0){
+          replaceUrl('/setup/campaign');
+        }
+      
+        if(activeCompany?.payment_integration_type !== null && activeCompany?.company_currency !== null && userCampaignDetails !== null && userCampaignDetails?.length > 0 && activeCompany?.domain_verified !== true){
+          replaceUrl('/setup/add');
+        }
     
-      if(activeCompany?.payment_integration_type !== null && activeCompany?.company_currency === null){
-        replaceUrl('/setup/currency');
-      }
-    
-      if(activeCompany?.payment_integration_type !== null && activeCompany?.company_currency !== null && userCampaignDetails?.length === 0){
-        replaceUrl('/setup/campaign');
-      }
-    
-      if(activeCompany?.payment_integration_type !== null && activeCompany?.company_currency !== null && userCampaignDetails !== null && userCampaignDetails?.length > 0 && activeCompany?.domain_verified !== true){
-        replaceUrl('/setup/add');
-      }
-  
-      if(activeCompany?.domain_verified === true){
-        replaceUrl('/setup/verify');
+        if(activeCompany?.domain_verified === true){
+          replaceUrl('/setup/verify');
+        }
       }
     }
-  }
+  }, [activeCompany]);
 }
